27474c31a8be23c3b32afde77a372ade77ff89c3
Enrico <enrico.golfieri@gmail.com>
2023-10-07 00:48:57 +0200

    removed docker alias duplicate

--- plugins/docker-extra/docker-extra.plugin.zsh
+++ plugins/docker-extra/docker-extra.plugin.zsh
@@ -66,23 +66,6 @@ function docker-setup-nvidia()
     sudo reboot
 }
 
-function docker-setup-nvidia()
-{
-    sudo rm -rf /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    sudo rm -rf /etc/apt/sources.list.d/nvidia-docker.list
-    sudo rm -rf /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-    
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-    sudo nvidia-ctk runtime configure --runtime=docker
-    sudo systemctl restart docker
-    sudo reboot
-}
-
 function docker-full-reset()
 {
    echo "Note, this will erease all your containers and images, continue? [y\Y]"
