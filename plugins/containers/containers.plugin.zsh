eea8ef0e805a34745839859c80440e8e031887fa
Enrico Golfieri <enrico.golfieri@mongodb.com>
2025-03-14 11:27:42 +0000

    Added dockers files per machine

--- plugins/containers/containers.plugin.zsh
+++ plugins/containers/containers.plugin.zsh
@@ -1,44 +1,8 @@
-function runlinda()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up linda -d
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec linda /bin/bash  # Open an interactive shell
-}
 
-function runlinda-gpu()
+function runpi()
 {
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up linda-gpu -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec linda-gpu /bin/bash  # Open an interactive shell
-}
-
-
-function runchiara()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up chiara -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec chiara /bin/bash  # Open an interactive shell
-}
-
-function runchiara-gpu()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up chiara-gpu -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec chiara-gpu /bin/bash  # Open an interactive shell
-}
-
-function runserena()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up serena -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec serena /bin/bash  # Open an interactive shell
-}
-
-function runserena-gpu()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up serena-gpu -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec serena-gpu /bin/bash  # Open an interactive shell
-}
-
-function runrasp()
-{
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up rasp -d 
-    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec rasp /bin/bash  # Open an interactive shell
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up pi -d 
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec pi /bin/bash  # Open an interactive shell
 }
 
 function runmaya()
@@ -53,8 +17,24 @@ function runmaya-gpu()
     docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec maya-gpu /bin/bash  # Open an interactive shell
 }
 
-alias run-ubuntu-18='runlinda'
-alias run-ubuntu-20='runchiara'
-alias run-ubuntu-22='runserena'
-alias run-rasp='runrasp'
-alias run-maya='runmaya'
+function build-mongovm()
+{
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up -d dev-mongo
+}
+
+function run-mongovm()
+{
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec dev-mongo /bin/bash
+}
+
+function runhome()
+{
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up home -d 
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec home /bin/bash  # Open an interactive shell
+}
+
+function runhomeserver()
+{
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml up home-server -d 
+    docker-compose -f $WS_PLUGINS_PATH/containers/docker-compose.ubuntu.yml exec home-server /bin/bash  # Open an interactive shell
+}
